b29349ee4f2204406344c623485a3e5cf627b7d3
Jimyung Park <pzy51992@hotmail.com>
2019-01-20 02:57:21 +0000

    add review controller, model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190118015449) do
+ActiveRecord::Schema.define(version: 20190120020858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,17 @@ ActiveRecord::Schema.define(version: 20190118015449) do
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "product_id"
+    t.string   "user_id"
+    t.string   "description"
+    t.string   "rating"
+    t.string   "createdat"
+    t.string   "updatedat"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
